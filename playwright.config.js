e4add1c4b1274215cd2f236f1619e4d88e4ad53a
kamidikrishna <kamidi.krishna@kellton.com>
2025-08-19 18:29:21 +0530

    Update Playwright config and add GitHub Actions workflow for WebKit

--- playwright.config.js
+++ playwright.config.js
@@ -1,23 +1,39 @@
 // @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-  testDir: './playwright-pom-demo/tests',   // 👈 your test folder
+const config = defineConfig({
+  testDir: './playwright-pom-demo/tests',
   timeout: 30000,
   retries: 0,
   reporter: [
-  ['list'],
-  ['html', { outputFolder: 'playwright-pom-demo/reports/html-report', open: 'never' }],
-  ['allure-playwright', { outputFolder: 'playwright-pom-demo/reports/allure-results' }]
-],
+    ['list'],
+    ['html', { outputFolder: 'playwright-pom-demo/reports/html-report', open: 'never' }],
+    ['allure-playwright', { outputFolder: 'playwright-pom-demo/reports/allure-results' }]
+  ],
 
-
-  use: {
-    headless: false,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    baseURL: 'https://rahulshettyacademy.com/loginpagePractise/',
-    browserName: 'chromium'
-  },
-};
+  projects: [
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: 'https://rahulshettyacademy.com/loginpagePractise/',
+        headless: false,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+      },
+    },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: 'https://rahulshettyacademy.com/loginpagePractise/',
+        headless: false,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+      },
+    },
+  ],
+});
 
 module.exports = config;
